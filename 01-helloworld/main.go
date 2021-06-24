4b290074189d7da18992fa621c973ef806e27475
andochiwa <a1066079469@gmail.com>
2021-06-24 19:25:35 +0900

    custom http handler

--- 01-helloworld/main.go
+++ 01-helloworld/main.go
@@ -6,12 +6,21 @@ import (
 )
 
 // 创建处理器函数
-func handler(response http.ResponseWriter, request *http.Request) {
-	_, _ = fmt.Fprintln(response, "hello world", request.URL.Path)
+//func handler(response http.ResponseWriter, request *http.Request) {
+//	_, _ = fmt.Fprintln(response, "hello world", request.URL.Path)
+//}
+
+// 使用自定义struct创建处理器方法
+type MyHandler struct {
+}
+
+func (handler *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintln(w, "custom handler")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	myHandler := MyHandler{}
+	http.Handle("/", &myHandler)
 
 	// 创建路由
 	_ = http.ListenAndServe(":8080", nil)
